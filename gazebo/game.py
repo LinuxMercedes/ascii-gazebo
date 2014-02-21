c67c473a3d50f1964f17e9046e06534c41150c14
LinuxMercedes <LinuxMercedes@gmail.com>
2014-02-20 21:48:07 -0600

    Rooms and basic movement

--- gazebo/game.py
+++ gazebo/game.py
@@ -1,22 +1,58 @@
 
 class Game:
-  done = False
+  """
+  The game object. Handles setup, user interaction, etc.
+  Subclass and implement additional methods to make your game.
+  """
 
-  def prompt(self):
-    command = input('> ')
+  done = False
+  room = None
+  prompt = '> '
+
+  def change_room(self, direction):
+    if self.room is None:
+      return "Oops, you're not on the map. You should get that checked out."
+
+    direction = direction[1:]
+    if direction in self.room.adjacent:
+      self.room = self.rooms[self.room.adjacent[direction]]
+      return None
+    else: 
+      return "Can't go that way."
+
+  def unknown_command(self, command):
+    return "Hey, " + command  + " isn't a command, jackass"
+
+  def get_input(self):
+    """ Prompt user for actions """
+    command = input(self.prompt)
     self.process_command(command)
 
   def process_command(self, command):
+    """ Process commands from the user and hand off to the derived actions """
     for key, action in self.commands.items():
       if command.startswith(key):
-        action(self, command[len(key):])
-        break
+        output = action(self, command[len(key):])
+        if output:
+          print(output)
+        return
+
+    output = self.unknown_command(command)
+    if output:
+      print(output)
 
   def update(self):
     pass
 
   def run(self):
     while(not self.done):
-      self.prompt()
+      self.get_input()
       self.update()
+
+class Room:
+  """ 
+  A Room. Tracks adjacent rooms and contents.
+  """
+  def __init__(self, nearby, items):
+    self.adjacent = nearby
 
--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from gazebo.game import Game
+from gazebo.room import Room
 
 def look(game, thing):
     print("Hah! You can't see", thing)
@@ -10,7 +11,35 @@ class mygame(Game):
     commands = {
         'look' : look,
         'quit' : quit,
+        'go' : Game.change_room,
+        'lel' : Game.unknown_command,
     }
+
+    rooms = {
+        'butt' : Room(
+            {
+                'north' : 'face',
+                'south' : 'ass',
+            },
+            {}
+        ),
+        'face' : Room(
+            {
+                'south' : 'butt',
+                'east' : 'ass',
+            },
+            {}
+        ),
+        'ass' : Room(
+            {
+                'north' : 'butt',
+                'west' : 'face',
+            },
+            {}
+        ),
+    }
+
+    room = rooms['butt']
 
 if __name__=="__main__":
     game = mygame()
