711e6cd61a67615458c0a7b1a0f64c94652bdf0d
LinuxMercedes <LinuxMercedes@gmail.com>
2014-03-01 21:32:24 -0600

    Documentation

--- gazebo/game.py
+++ gazebo/game.py
@@ -7,11 +7,17 @@ class Game:
     Subclass and implement additional methods to make your game.
     """
 
-    done = False
-    room = None
-    prompt = '> '
+    done = False # Set to True to end the game
+    room = None # Contains a Room that is the current room
+    prompt = '> ' 
     
     def __init__(self, player, commands, rooms, room):
+        """
+        player: An instance of Player.
+        commands: A dict of ("typed command", function_name). Look in command.py for more details on command functions.
+        rooms: A dict of ("short name", Room instance). The short name is used to refer to rooms internally.
+        room: A Room instance, probably from rooms. This is the room where you start.
+        """
         self.player = player
         self.commands = commands
         self.rooms = rooms
@@ -40,9 +46,11 @@ class Game:
         self.last_commnand = (unknown_command, '', command)
 
     def start(self):
+        """ Called when the game starts. """
         pass
 
     def update(self):
+        """ Called after every command. Use to update state and move stuff around """
         pass
 
     def run(self):
@@ -55,11 +63,18 @@ class Room:
     """ 
     A Room. Tracks description, adjacent rooms, and contents.
     """
+    
     def __init__(self, description, nearby, items, npcs):
+        """
+        description: The room description, printed out every time you look or enter a room.
+        nearby: Nearby rooms. A dict of ("move command", "room short name").
+        items: An array of Item objects that are in the room. 
+        npcs: An array of NPC objects that are in the room.
+        """
+
         self.description = description
         self.nearby = nearby
         self.items = self._make_dict(items)
-        #self.npcs = self._make_dict(npcs)
         self.npcs = npcs
 
     def _make_dict(self, things):
@@ -69,6 +84,7 @@ class Room:
         return d
 
     def describe(self):
+        """ Describe the room, using nearby rooms, items, and npcs """
         ret = ['You are in ', self.description]
 
         if len(self.nearby):
@@ -101,6 +117,10 @@ class Item:
     An item. 
     """
     def __init__(self, name, description):
+        """
+        name: The item name
+        description: The description of the item
+        """
         self.name = name
         self.description = description
 
@@ -110,6 +130,9 @@ class Player:
     """
 
     def __init__(self, inventory = {}):
+        """ 
+        inventory: a dict of ("name", Item) pairs that the player has in their inventory
+        """
         self.inventory = inventory
 
 class NPC:
@@ -117,6 +140,15 @@ class NPC:
     An NPC or enemy
     """
     def __init__(self, name, description, language):
+        """
+        name: The name of the npc.
+        description: A description of the NPC. 
+        language: A list of 3-tuples of ("regex", "response", True/False). 
+            Regexes are matched in order. 
+            The response is expanded with match groups from the regex, so it is possible to rudimentarily adaptively talk to the user.
+            If the regex matches and the 3rd value is False, no more NPCs process the message. Otherwise, other NPCs in the room may receive the message.
+        """
+
         self.name = name
         self.description = description
         self.language = []
