520dd97be52e65f854041c8de92f15b8fa554838
LinuxMercedes <LinuxMercedes@gmail.com>
2014-03-09 23:23:21 -0500

    Packaging code

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ ascii-gazebo
 
 Text Adventure framework in Python
 
-
 Context
 =======
 http://web.archive.org/web/20080804140516/http://www.dreadgazebo.com/index.php?name=News&file=article&sid=8
---
+++ setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(
+    name='ascii-gazebo',
+    version='0.1',
+    packages=['ascii-gazebo',],
+    license='MIT License',
+    long_description=open('README.md').read(),
+)
