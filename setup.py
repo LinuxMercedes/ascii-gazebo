e44eb89800a9c73ae08c2a505c7f7f4007fda90d
LinuxMercedes <LinuxMercedes@gmail.com>
2014-03-10 00:31:17 -0500

    Releasing a bugfix

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='ascii_gazebo',
-    version='0.1.2',
+    version='0.1.3',
     packages=['ascii_gazebo',],
     license='MIT License',
     long_description=open('README.md').read(),
