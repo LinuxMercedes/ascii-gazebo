160c61b6a4dbadec4c94c4db185d050356eab1a4
LinuxMercedes <LinuxMercedes@gmail.com>
2014-03-09 23:37:40 -0500

    Adding more info that will eventually show up

--- setup.py
+++ setup.py
@@ -6,4 +6,8 @@ setup(
     packages=['ascii_gazebo',],
     license='MIT License',
     long_description=open('README.md').read(),
+    author="LinuxMercedes",
+    author_email="linuxmercedes@gmail.com",
+    home_page="https://github.com/LinuxMercedes/ascii-gazebo",
+    summary="Text Adventure framework in python",
 )
